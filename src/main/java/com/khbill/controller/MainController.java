49d1b1e9ae61063bea46b44bbd73bbdc3ba382b6
over-match <86829837+over-match@users.noreply.github.com>
2021-11-10 23:14:40 +0900

    테스트

--- src/main/java/com/khbill/controller/MainController.java
+++ src/main/java/com/khbill/controller/MainController.java
@@ -1,14 +1,18 @@
 package com.khbill.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
 	@RequestMapping(value="/main")
 	public void main() {
-		
+		logger.info("메인 테스트");
 	}
 	
 }
