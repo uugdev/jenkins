cc87f1c4a2542a364afe8e5ef8ec427e7ff0ca5a
jiwonee22 <jiwonee22@naver.com>
2021-11-13 23:14:57 +0900

    feat: mypage 결제 체크 리스트 구현

---
+++ src/main/java/com/khbill/controller/MypageController.java
@@ -0,0 +1,62 @@
+package com.khbill.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.khbill.dto.Ask;
+import com.khbill.dto.Item;
+import com.khbill.dto.Review;
+import com.khbill.dto.Vote;
+import com.khbill.service.face.AskService;
+import com.khbill.util.Paging;
+
+@Controller
+public class MypageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
+	
+	@Autowired AskService askService;
+
+	@RequestMapping(value="/mypage/item/list")
+	public void itemList(HttpSession session, Model model, Paging paramData) {
+		logger.info("/mypage/item/list [GET]");
+		
+		int userNo = (int)session.getAttribute("userNo");
+		logger.info("세션에 저장된 유저 번호 : {}", userNo);
+		
+		Paging paging = askService.getPaging(paramData, userNo);
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("userNo", userNo);
+		map.put("paging", paging);
+		
+		List<Ask> askList = askService.getAskItemList(map);
+		List<Item> itemList = askService.getItemList(userNo);
+		List<Vote> voteList = askService.getVoteList(userNo);
+		List<Review> reviewList = askService.getReviewList(userNo);
+		
+		model.addAttribute("ask", askList);
+		model.addAttribute("item", itemList);
+		model.addAttribute("vote", voteList);
+		
+		logger.info("askList 정보 : {}", askList);
+		logger.info("itemList 정보 : {}", itemList);
+		logger.info("voteList 정보 : {}", voteList);
+
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/mypage/item/list.jsp
@@ -0,0 +1,63 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<c:import url="/WEB-INF/views/layout/head.jsp" />
+<c:import url="/WEB-INF/views/layout/header.jsp" />
+<!-- header end -->
+
+<!-- 개별 스타일 및 스크립트 영역 -->
+
+<!-- 개별 영역 끝 -->
+
+<div class="wrap">
+<div class="container">
+
+<h1>결제 체크 리스트</h1>
+
+<table class="table table-hover">
+	<tr>
+		<th>번호</th>
+		<th>상태</th>
+		<th>상품명</th>
+		<th>가격</th>
+		<th>마감일</th>
+		<th>조회수</th>
+		<th>변경하기</th>
+	</tr>
+	<c:forEach items="${item }" var="item" varStatus="status">
+	<tr>
+		<td>${status.count }</td>
+		<td>${item.itemStatus }</td>
+		<td>${item.itemName }</td>
+		<td>${item.itemPrice }</td>
+		<c:forEach items="${vote }" var="vote">
+			<c:if test="${item.userNo eq vote.userNo}">
+				<td><fmt:formatDate value="${vote.voteEnd }" pattern="yy-MM-dd" /></td>
+			</c:if>
+		</c:forEach>
+			<c:forEach items="${ask }" var="ask">
+				<c:if test="${item.userNo eq ask.userNo}">
+					<td>${ask.askHit}</td>
+				</c:if>
+		</c:forEach>
+		<c:if test="${item.itemStatus == 'n' }">
+			<td><button>결제로 변경하기</button></td>
+		</c:if>
+		<c:if test="${item.itemStatus == 'y' }">
+			<td><button>후기 작성하기</button></td>
+		</c:if>
+	</tr>
+	</c:forEach>
+</table>
+
+
+
+</div><!-- .container end -->
+</div><!-- .wrap end -->
+<c:import url="/WEB-INF/views/layout/paging.jsp" />
+
+<!-- footer start -->
+<c:import url="/WEB-INF/views/layout/footer.jsp" />
+
